2953688bb6fae2dfcff191cce09b1a967679acda
Tuffk <tuffkmulhall@gmail.com>
2017-09-26 18:37:49 -0500

    masks

--- zain.py
+++ zain.py
@@ -6,7 +6,25 @@ from sklearn import tree
 from sklearn import preprocessing
 
 
+def cast(board):
+    num = 0
+    for i in board:
+        for j in i:
+            num = (num*10) + j
+    return num
+
 def main():
+    global masks
+    masks = [
+            100000110000001, # T palito izquierda
+            1110000010, # T normal
+            100000111, # T invertida 
+            1000000110000010, # T palito derecha
+            100000100000101,# T (45) inferior derecha
+            10100000100000001,# T (135) superior derecha
+            10000000100000101, # T (-45) inferior izquierda
+            10100000100000100# T (-135) superior izquierda
+            ]
     print("Hola Mundo!")
 
 
