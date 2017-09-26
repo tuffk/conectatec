0ae9dc052fac9d2e763324d6e3fc0275c47d5220
Tuffk <tuffkmulhall@gmail.com>
2017-09-26 16:56:24 -0500

    initial

---
+++ zain.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import os
+from sklearn import tree
+from sklearn import preprocessing
+
+
+def main():
+    print("Hola Mundo!")
+
+
+if __name__ == "__main__":
+    main()
