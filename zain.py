710122c172f710a5a52ee0aadd702dcace216cfc
Tuffk <tuffkmulhall@gmail.com>
2017-09-26 19:32:34 -0500

    wise

--- zain.py
+++ zain.py
@@ -13,6 +13,23 @@ def cast(board):
             num = (num*10) + j
     return num
 
+def wise(num, mask):
+    res = 0
+    pos = []
+    cont = 0
+    while(mask > 0):
+        if((mask % 10 ) == 0):
+            continue
+        if ((mask % 10) == (num % 10)):
+            res += .25
+        elif((num % 10) > 0):
+            return -1,[]
+        else:
+            pos.append(cont)
+        mask /= 10
+        cont += 1
+    return res,pos
+
 def main():
     global masks
     masks = [
@@ -25,7 +42,14 @@ def main():
             10000000100000101, # T (-45) inferior izquierda
             10100000100000100# T (-135) superior izquierda
             ]
-    print("Hola Mundo!")
+    #zain = int(bin(11) & bin(11))
+    print(100001 & 10001)
+
+def cehckMask():
+    pass
+
+def checPosible():
+    pass
 
 
 if __name__ == "__main__":
